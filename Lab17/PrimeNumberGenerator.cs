2cc324a3893dc0f6f1ddef3ece045e9ac720ab5a
Jake Balch <InariOrchid@gmail.com>
2019-05-17 01:59:48 -0400

    lab17

---
+++ Lab17/PrimeNumberGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    public class PrimeNumberGenerator
+    {
+        public static int GetPrimeNumber(int n)
+        {
+            List<int> list = new List<int>();
+            list.Add(2);
+            int nextPrime = 3;
+            while(list.Count < n )
+            {
+                int sqrt = (int)Math.Sqrt(nextPrime);
+                bool isPrime = true;
+                for(int i=0; list[i]<=sqrt;i++)
+                {
+                    if(nextPrime % list[i] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                }
+                if (isPrime)
+                {
+                    list.Add(nextPrime);
+
+                }
+                nextPrime+= 2;
+            }
+            return list[n - 1];
+            
+        }
+    }
+}
---
+++ Lab17/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool proceed = true;
+            bool intIsSuccess = false;
+
+            Console.WriteLine("Let's locate some primes!\n");
+            Console.WriteLine("This application will find you any prime, in order, from the first prime number on.");
+
+            while (proceed)
+            {
+                intIsSuccess = false;
+                int getPrime = 0;
+                while (!intIsSuccess)
+                {
+                    Console.Write("Which prime number are you looking for? ");
+                    intIsSuccess = int.TryParse(Console.ReadLine(), out getPrime);
+                }
+                
+                int myPrime = PrimeNumberGenerator.GetPrimeNumber(getPrime);
+                Console.WriteLine($"The number {getPrime} prime is {myPrime}.\n");
+                Console.Write("Do you want to find another prime number? (y/n): ");
+                string response = Console.ReadLine().ToLower();
+                if (response == "y")
+                {
+                    proceed = true;
+                }
+                else
+                {
+                    proceed = false;
+                }
+            }
+
+            Console.WriteLine("Thank you and good-bye!");
+           
+
+        }
+    }
+}
